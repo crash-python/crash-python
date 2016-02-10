631deb0025b86671121046eeeb9ee984859125de
Jeff Mahoney <jeffm@suse.com>
2016-02-10 16:40:07 -0500

    crash.arch: introduce and use architecture-specific thread handling
    This commit defines a CrashArchitecture base class for
    architecture-specific code.  We use it for implementing initial x86_64
    support for thread handling.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

---
+++ crash/arch/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+class CrashArchitecture:
+    ident = "base-class"
+    aliases = None
+    def __init__(self):
+        pass
+
+    def setup_thread(self, thread):
+        raise NotImplementedError("setup_thread not implemented")
+
+architectures = {}
+def register(arch):
+    architectures[arch.ident] = arch
+    for ident in arch.aliases:
+        architectures[ident] = arch
+
+def get_architecture(archname):
+    if archname in architectures:
+        return architectures[archname]
+
+    return None
