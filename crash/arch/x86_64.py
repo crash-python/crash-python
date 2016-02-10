3c058b656c47c7028c07abea278808dc14343468
Jeff Mahoney <jeffm@suse.com>
2016-02-10 16:40:07 -0500

    crash.arch: introduce and use architecture-specific thread handling
    This commit defines a CrashArchitecture base class for
    architecture-specific code.  We use it for implementing initial x86_64
    support for thread handling.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

---
+++ crash/arch/x86_64.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+import gdb
+from crash.arch import CrashArchitecture, register
+
+class x86_64Architecture(CrashArchitecture):
+    ident = "i386:x86-64"
+    aliases = [ "x86_64" ]
+
+    def __init__(self):
+        # PC for blocked threads
+        self.rip = gdb.lookup_minimal_symbol("thread_return").value()
+        self.ulong_type = gdb.lookup_type('unsigned long')
+
+    def setup_thread(self, task):
+        ulong_type = self.ulong_type
+
+        rsp = task['thread']['sp'].cast(ulong_type.pointer())
+        rbp = rsp.dereference().cast(ulong_type.pointer())
+        rbx = (rbp - 1).dereference()
+        r12 = (rbp - 2).dereference()
+        r13 = (rbp - 3).dereference()
+        r14 = (rbp - 4).dereference()
+        r15 = (rbp - 5).dereference()
+
+        # The two pushes that don't have CFI info
+        # rsp += 2
+
+        # ex = in_exception_stack(rsp)
+        # if ex:
+        #     print "EXCEPTION STACK: pid %d" % task['pid']
+
+        thread.registers['rsp'].value = rsp
+        thread.registers['rbp'].value = rbp
+        thread.registers['rip'].value = self.rip
+        thread.registers['rbx'].value = rbx
+        thread.registers['r12'].value = r12
+        thread.registers['r13'].value = r13
+        thread.registers['r14'].value = r14
+        thread.registers['r15'].value = r15
+        thread.registers['cs'].value = 2*8
+        thread.registers['ss'].value = 3*8
+
+register(x86_64Architecture)
--- crash/kdump/target.py
+++ crash/kdump/target.py
@@ -7,44 +7,11 @@ from __future__ import division
 
 import gdb
 from kdumpfile import kdumpfile, KDUMP_KVADDR
-from crash.types.util import list_for_each_entry
 from kdumpfile.exceptions import *
+from crash.types.list import list_for_each_entry
 
-#arch = "i386:x86-64"
-#
-#setup = {
-#    'i386:x86-64' : setup_thread_amd64,
-#}
-
-ulong_type = gdb.lookup_type('unsigned long')
-rip = gdb.lookup_minimal_symbol("thread_return").value()
-
-def setup_thread_amd64(thread, task):
-    rsp = task['thread']['sp'].cast(ulong_type.pointer())
-    rbp = rsp.dereference().cast(ulong_type.pointer())
-    rbx = (rbp - 1).dereference()
-    r12 = (rbp - 2).dereference()
-    r13 = (rbp - 3).dereference()
-    r14 = (rbp - 4).dereference()
-    r15 = (rbp - 5).dereference()
-
-    # The two pushes that don't have CFI info
-#    rsp += 2
-
-#    ex = in_exception_stack(rsp)
-#    if ex:
-#    print("EXCEPTION STACK: pid {:d}".format(task['pid']))
-
-    thread.registers['rsp'].value = rsp
-    thread.registers['rbp'].value = rbp
-    thread.registers['rip'].value = rip
-    thread.registers['rbx'].value = rbx
-    thread.registers['r12'].value = r12
-    thread.registers['r13'].value = r13
-    thread.registers['r14'].value = r14
-    thread.registers['r15'].value = r15
-    thread.registers['cs'].value = 2*8
-    thread.registers['ss'].value = 3*8
+import crash.arch
+import crash.arch.x86_64
 
 def symbol_func(symname):
     ms = gdb.lookup_minimal_symbol(symname)
@@ -60,11 +27,20 @@ class Target(gdb.Target):
             self.fil = fil
             print("kdump ({})".format(fil))
             self.kdump = kdumpfile(fil)
+            self.setup_arch()
             self.kdump.symbol_func = symbol_func
             self.kdump.vtop_init()
             super(Target, self).__init__()
             gdb.execute('set print thread-events 0')
             self.setup_tasks()
+
+    def setup_arch(self):
+        archname = self.kdump.attr("arch")['name']
+        archclass = crash.arch.get_architecture(archname)
+        if not archclass:
+            raise NotImplementedError("Architecture %s is not supported yet." % archname)
+
+        self.arch = archclass()
 
     def setup_tasks(self):
         init_task = gdb.lookup_global_symbol('init_task')
@@ -101,7 +77,7 @@ class Target(gdb.Target):
 
     def to_fetch_registers(self, register):
         thread = gdb.selected_thread()
-        setup_thread_amd64(thread, thread.info)
+        self.arch.setup_thread(thread)
         return True
 
     def to_prepare_to_store(self, thread):
