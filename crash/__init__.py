8696a498d768b352a6734473259cb42491965497
Jeff Mahoney <jeffm@suse.com>
2016-02-11 22:46:54 -0500

    crash.Session: add simple driver for crash interface
    Initially, it just accepts a filename to set up a target.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

--- crash/__init__.py
+++ crash/__init__.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python
 # vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
+
+import kdump.target
+
+class Session:
+    def __init__(self, filename):
+        self.target = kdump.target.Target(filename)
