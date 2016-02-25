43360e03060664983324d0d919e2bd7c284fecd6
Jeff Mahoney <jeffm@suse.com>
2016-02-25 08:45:12 -0500

    crash: delay importing kdump until we're instantiated
    This allows us to load the module whenever, but wait until we have
    the executable environment set up.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

--- crash/__init__.py
+++ crash/__init__.py
@@ -2,8 +2,8 @@
 # vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
 
 import kdump.target
-import crash.commands
 
 class Session:
     def __init__(self, filename):
         self.target = kdump.target.Target(filename)
+        import crash.commands
