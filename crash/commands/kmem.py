bfad3cdfd23a5318593be33f272b496cc8ea95b4
Vlastimil Babka <vbabka@suse.cz>
2016-09-09 15:27:51 +0200

    pykmem: provide basic address query
    Teach pykmem command to report if given address is inside a slab object, and
    whether it's allocated or free (including array caches). The -s param is now
    for checking individual cache given by name, or all caches.
    
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>

--- crash/commands/kmem.py
+++ crash/commands/kmem.py
@@ -2,6 +2,7 @@
 # vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
 
 import gdb
+import crash
 from crash.commands import CrashCommand
 from crash.cache import slab
 from crash.types.slab import KmemCache, Slab
@@ -12,7 +13,14 @@ class KmemCommand(CrashCommand):
     """ kernel memory inspection
 
 NAME
-  TODO
+  kmem - kernel memory inspection
+
+SYNOPSIS
+  kmem addr             - try to find addr within kmem caches
+  kmem -s [slabname]    - check consistency of single or all kmem cache
+
+DESCRIPTION
+  This command currently offers very basic kmem cache query and checking.
     """
 
     def __init__(self, name):
@@ -27,13 +35,56 @@ NAME
 
     def execute(self, args):
         if args.s:
-            cache_name = args.arg[0]
-            cache = KmemCache.from_name(cache_name)
-            cache.check_all()
-            return
-            
-        for cache in slab.cache.get_kmem_caches().values():
-            print cache.name
-            cache.check_all()
+            if args.arg:
+                cache_name = args.arg[0]
+                print "Checking kmem cache " + cache_name
+                cache = KmemCache.from_name(cache_name)
+                cache.check_all()
+            else:
+                print "Checking all kmem caches..."  
+                for cache in crash.cache.slab.cache.get_kmem_caches().values():
+                    print cache.name
+                    cache.check_all()
 
+            print "Checking done."
+            return
+          
+        if not args.arg:
+            print "Nothing to do."
+            return
+
+        addr = long(args.arg[0], 0)
+        slab = Slab.from_obj(addr)
+
+        if not slab:
+            print "Address not found in any kmem cache."
+            return
+
+        obj = slab.contains_obj(addr)
+        name = slab.kmem_cache.name
+
+        if obj[0]:
+            print ("ALLOCATED object %x from slab %s" % (obj[1], name))
+        else:
+            if obj[1] == 0L:
+                print ("Address on slab %s but not within valid object slot"
+                                % name)
+            elif not obj[2]:
+                print ("FREE object %x from slab %s" % (obj[1], name))
+            else:
+                ac = obj[2]
+                if ac["ac_type"] == "percpu":
+                    ac_desc = "cpu %d cache" % ac["nid_tgt"]
+                elif ac["ac_type"] == "shared":
+                    ac_desc = "shared cache on node %d" % ac["nid_tgt"]
+                elif ac["ac_type"] == "alien":
+                    ac_desc = "alien cache of node %d for node %d" % (ac["nid_src"], ac["nid_tgt"])
+                else:
+                    print "unexpected array cache type"
+                    print ac
+                    return
+                    
+                print ("FREE object %x from slab %s (in %s)" %
+                                            (obj[1], name, ac_desc))
+                
 KmemCommand("kmem")
