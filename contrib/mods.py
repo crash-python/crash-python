d0ac38b079b5e2ddb28608235d0961b5831ae8be
Jeff Mahoney <jeffm@suse.com>
2016-10-06 09:59:14 -0400

    contrib: add contrib directory for one-off scripts
    This commit adds a contrib directory to hold scripts that may not be
    ready to be incorporated into the main crash project.
    
    Initially it contains 'mods.py' that loads all modules from
    /lib/modules/$UNAME_R/kernel and 'lsbtrfs-cmd.py' that is a demo for
    adding a quick command.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

---
+++ contrib/mods.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
+
+from crash.types.list import list_for_each_entry
+import gdb
+import uuid
+from crash.cache import sys
+import os
+
+def find(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+
+sys.cache.init_sys_caches()
+path = "/lib/modules/{}".format(sys.cache.utsname_cache['release'])
+
+modules = gdb.lookup_symbol('modules', None)[0].value()
+module_type = gdb.lookup_type('struct module')
+for module in list_for_each_entry(modules, module_type, 'list'):
+    modname = "{}.ko".format(module['name'].string())
+    modpath = find(modname, path)
+    if not modpath and modname.find('_') != -1:
+        modname = modname.replace('_', '-')
+        modpath = find(modname, path)
+    if not modpath:
+        print "Couldn't find {} under {}.".format(module['name'], path)
+        continue
+    gdb.execute("add-symbol-file {} {}".format(modpath, module['module_core']))
