eb5feec0f1d0d274d6acdcaf26f84e085e62af7c
Jeff Mahoney <jeffm@suse.com>
2016-10-06 09:59:14 -0400

    contrib: add contrib directory for one-off scripts
    This commit adds a contrib directory to hold scripts that may not be
    ready to be incorporated into the main crash project.
    
    Initially it contains 'mods.py' that loads all modules from
    /lib/modules/$UNAME_R/kernel and 'lsbtrfs-cmd.py' that is a demo for
    adding a quick command.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

---
+++ contrib/lsbtrfs-cmd.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
+
+path = "/lib/modules/4.4.20-6.gd2e08c5-default/kernel/fs/btrfs/btrfs.ko"
+
+from crash.types.list import list_for_each_entry
+import gdb
+import uuid
+from crash.commands import CrashCommand
+
+class LSBtrfs(CrashCommand):
+    """
+    doc
+    """
+    def __init__(self):
+        CrashCommand.__init__(self, "lsbtrfs", None)
+
+
+    def execute(self, argv):
+        super_blocks = gdb.lookup_symbol('super_blocks', None)[0].value()
+        sbtype = gdb.lookup_type('struct super_block')
+
+        try:
+            btrfs_fs_info_type = gdb.lookup_type('struct btrfs_fs_info')
+        except gdb.error:
+            # Load the module if it's not loaded yet
+            module_type = gdb.lookup_type('struct module')
+
+            modules = gdb.lookup_symbol('modules', None)[0].value()
+            for module in list_for_each_entry(modules, module_type, 'list'):
+                if module['name'].string() == "btrfs":
+                    addr = module['module_core']
+                    gdb.execute("add-symbol-file {} {}".format(path, addr))
+            btrfs_fs_info_type = gdb.lookup_type('struct btrfs_fs_info')
+
+        for sb in list_for_each_entry(super_blocks, sbtype, 's_list'):
+            if sb['s_type']['name'].string() == "btrfs":
+                fs_info = gdb.Value(sb['s_fs_info']).cast(btrfs_fs_info_type.pointer())
+
+                u = long(0)
+                for i in range(0, 16):
+                    u <<= 8
+                    u += int(fs_info['fsid'][i])
+                u = uuid.UUID(int=u)
+                print "{} -> {} {}".format(sb.address, sb['s_id'].string(), u)
+
+LSBtrfs()
