9a70781be020158640b6930f66ac6128cdb5a1e1
Jeff Mahoney <jeffm@suse.com>
2017-03-04 10:50:21 -0500

    crash: don't import until needed
    We can import the target module when we instantiate the session.  Otherwise
    we load everything for things like unit tests that don't depend on
    the rest of the infrastructure.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

--- crash/__init__.py
+++ crash/__init__.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
 
-import crash.kdump.target
-from crash import kdump
 
 class Session:
     def __init__(self, filename):
-        self.target = kdump.target.Target(filename)
+        import crash.kdump.target
+        self.target = crash.kdump.target.Target(filename)
         import crash.commands
