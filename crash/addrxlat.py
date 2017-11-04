359e6caec114a3e3c185c14ebc194164941488e2
Petr Tesarik <ptesarik@suse.com>
2017-11-04 19:59:50 +0100

    vtop: Initial implementation of the "vtop" command
    Add a crash module for generic address translation and use it for a
    "vtop" command. This is a minimal implementation which shows only
    the resulting physical address, not the translation steps.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

---
+++ crash/addrxlat.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+import gdb
+import sys
+
+if sys.version_info.major >= 3:
+    long = int
+
+import addrxlat
+from crash.infra import CrashBaseClass, export
+from crash.cache.syscache import utsname
+from crash.util import offsetof
+
+class TranslationContext(addrxlat.Context):
+    def __init__(self, *args, **kwargs):
+        super(TranslationContext, self).__init__(*args, **kwargs)
+        self.read_caps = addrxlat.CAPS(addrxlat.KVADDR)
+        self.uint32_ptr = gdb.lookup_type('uint32_t').pointer()
+        self.uint64_ptr = gdb.lookup_type('uint64_t').pointer()
+
+    def cb_sym(self, symtype, *args):
+        if symtype == addrxlat.SYM_VALUE:
+            ms = gdb.lookup_minimal_symbol(args[0])
+            if ms is not None:
+                return long(ms.value().address)
+        elif symtype == addrxlat.SYM_SIZEOF:
+            sym = gdb.lookup_symbol(args[0], None)[0]
+            if sym is not None:
+                return sym.type.sizeof
+        elif symtype == addrxlat.SYM_OFFSETOF:
+            sym = gdb.lookup_symbol(args[0], None, gdb.SYMBOL_STRUCT_DOMAIN)[0]
+            if sym is None:
+                # this works for typedefs:
+                sym = gdb.lookup_symbol(args[0], None)[0]
+            if sym is not None:
+                return offsetof(sym.type, args[1])
+
+        return super(TranslationContext, self).cb_sym(symtype, *args)
+
+    def cb_read32(self, faddr):
+        return long(gdb.Value(faddr.addr).cast(self.uint32_ptr).dereference())
+
+    def cb_read64(self, faddr):
+        return long(gdb.Value(faddr.addr).cast(self.uint64_ptr).dereference())
+
+class CrashAddressTranslation(CrashBaseClass):
+    def __init__(self):
+        self.context = TranslationContext()
+        self.system = addrxlat.System()
+        self.system.os_init(self.context,
+                            arch = utsname.machine,
+                            type = addrxlat.OS_LINUX)
+
+    @export
+    def addrxlat_context(self):
+        return self.context
+
+    @export
+    def addrxlat_system(self):
+        return self.system
---
+++ crash/commands/vtop.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
+
+from __future__ import print_function
+
+import gdb
+import argparse
+from crash.commands import CrashCommand, CrashCommandParser
+from crash.addrxlat import addrxlat_context, addrxlat_system
+import addrxlat
+
+class VTOPCommand(CrashCommand):
+    """convert virtual address to physical
+
+NAME
+  vtop - virtual to physical
+
+SYNOPSIS
+  vtop [-c [pid | taskp]] [-u|-k] address ...
+
+DESCRIPTION
+  This command translates a user or kernel virtual address to its physical
+  address.  Also displayed is the PTE translation, the vm_area_struct data
+  for user virtual addresses, the mem_map page data associated with the
+  physical page, and the swap location or file location if the page is
+  not mapped.  The -u and -k options specify that the address is a user
+  or kernel virtual address; -u and -k are not necessary on processors whose
+  virtual addresses self-define themselves as user or kernel.  User addresses
+  are translated with respect to the current context unless the -c option
+  is used.  Kernel virtual addresses are translated using the swapper_pg_dir
+  as the base page directory unless the -c option is used.
+
+   -u                 The address is a user virtual address; only required
+                      on processors with overlapping user and kernel virtual
+                      address spaces.
+   -k                 The address is a kernel virtual address; only required
+                      on processors with overlapping user and kernel virtual
+                      address spaces.
+   -c [pid | taskp]   Translate the virtual address from the page directory
+                      of the specified PID or hexadecimal task_struct pointer.
+                      However, if this command is invoked from "foreach vtop",
+                      the pid or taskp argument should NOT be entered; the
+                      address will be translated using the page directory of
+                      each task specified by "foreach".
+   address            A hexadecimal user or kernel virtual address.
+
+EXAMPLES
+  Translate user virtual address 80b4000:
+
+    crash> vtop 80b4000
+    VIRTUAL   PHYSICAL
+    80b4000   660f000
+
+    PAGE DIRECTORY: c37f0000
+      PGD: c37f0080 => e0d067
+      PMD: c37f0080 => e0d067
+      PTE: c0e0d2d0 => 660f067
+     PAGE: 660f000
+
+      PTE    PHYSICAL  FLAGS
+    660f067   660f000  (PRESENT|RW|USER|ACCESSED|DIRTY)
+
+      VMA      START      END      FLAGS  FILE
+    c773daa0   80b4000   810c000    77
+
+      PAGE    PHYSICAL   INODE     OFFSET  CNT FLAGS
+    c0393258   660f000         0     17000  1  uptodate
+
+  Translate kernel virtual address c806e000, first using swapper_pg_dir
+  as the page directory base, and secondly, using the page table base
+  of PID 1359:
+
+    crash> vtop c806e000
+    VIRTUAL   PHYSICAL
+    c806e000  2216000
+
+    PAGE DIRECTORY: c0101000
+      PGD: c0101c80 => 94063
+      PMD: c0101c80 => 94063
+      PTE: c00941b8 => 2216063
+     PAGE: 2216000
+
+      PTE    PHYSICAL  FLAGS
+    2216063   2216000  (PRESENT|RW|ACCESSED|DIRTY)
+
+      PAGE    PHYSICAL   INODE     OFFSET  CNT FLAGS
+    c02e9370   2216000         0         0  1
+
+    crash> vtop -c 1359 c806e000
+    VIRTUAL   PHYSICAL
+    c806e000  2216000
+
+    PAGE DIRECTORY: c5caf000
+      PGD: c5cafc80 => 94063
+      PMD: c5cafc80 => 94063
+      PTE: c00941b8 => 2216063
+     PAGE: 2216000
+
+      PTE    PHYSICAL  FLAGS
+    2216063   2216000  (PRESENT|RW|ACCESSED|DIRTY)
+
+      PAGE    PHYSICAL   INODE     OFFSET  CNT FLAGS
+    c02e9370   2216000         0         0  1
+
+  Determine swap location of user virtual address 40104000:
+
+    crash> vtop 40104000
+    VIRTUAL   PHYSICAL
+    40104000  (not mapped)
+
+    PAGE DIRECTORY: c40d8000
+      PGD: c40d8400 => 6bbe067
+      PMD: c40d8400 => 6bbe067
+      PTE: c6bbe410 => 58bc00
+
+     PTE      SWAP     OFFSET
+    58bc00  /dev/sda8   22716
+
+      VMA      START      END     FLAGS  FILE
+    c7200ae0  40104000  40b08000    73
+
+    SWAP: /dev/sda8  OFFSET: 22716
+    """
+
+    def __init__(self):
+        parser = CrashCommandParser(prog="vtop")
+
+        group = parser.add_mutually_exclusive_group()
+        group.add_argument('-u', action='store_true', default=False)
+        group.add_argument('-k', action='store_true', default=False)
+
+        parser.add_argument('-c', action='store_true', default=False)
+
+        parser.add_argument('args', nargs=argparse.ONE_OR_MORE)
+
+        parser.format_usage = lambda : \
+        "vtop [-c [pid | taskp]] [-u|-k] address ...\n"
+
+        super(VTOPCommand, self).__init__("vtop", parser)
+
+    def execute(self, argv):
+        ctx = addrxlat_context()
+        sys = addrxlat_system()
+        for addr in argv.args:
+            addr = int(addr, 16)
+            fulladdr = addrxlat.FullAddress(addrxlat.KVADDR, addr)
+            print('{:16}  {:16}'.format('VIRTUAL', 'PHYSICAL'))
+            try:
+                fulladdr.conv(addrxlat.KPHYSADDR, ctx, sys)
+                phys = '{:x}'.format(fulladdr.addr)
+            except addrxlat.BaseException:
+                phys = '---'
+            print('{:<16x}  {:<16}\n'.format(addr, phys))
+
+VTOPCommand()
