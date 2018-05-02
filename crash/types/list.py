8646f1cedb5b8eb6978961370ab26adb99fadd4a
Vlastimil Babka <vbabka@suse.cz>
2018-05-02 15:42:57 +0200

    types/list: support iterating lists in reverse order
    Add an optional reverse=False parameter for that.
    
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>

--- crash/types/list.py
+++ crash/types/list.py
@@ -26,7 +26,7 @@ class TypesListClass(CrashBaseClass):
     __types__ = [ 'struct list_head' ]
 
     @export
-    def list_for_each(self, list_head, include_head=False):
+    def list_for_each(self, list_head, include_head=False, reverse=False):
         pending_exception = None
         if isinstance(list_head, gdb.Symbol):
             list_head = list_head.value()
@@ -42,14 +42,20 @@ class TypesListClass(CrashBaseClass):
         if long(list_head.address) == 0:
             raise CorruptListError("list_head is NULL pointer.")
 
+        next_ = 'next'
+        prev_ = 'prev'
+        if reverse:
+            next_ = 'prev'
+            prev_ = 'next'
+
         if include_head:
             yield list_head.address
 
         try:
-            nxt = list_head['next']
+            nxt = list_head[next_]
             prev = list_head
             if long(nxt) == 0:
-                raise CorruptListError("next pointer is NULL")
+                raise CorruptListError("{} pointer is NULL".format(next_))
             node = nxt.dereference()
         except gdb.error as e:
             raise BufferError("Failed to read list_head {:#x}: {}"
@@ -59,39 +65,39 @@ class TypesListClass(CrashBaseClass):
             yield node.address
 
             try:
-                if long(prev.address) != long(node['prev']):
-                    error = ("broken prev link {:#x} -next-> {:#x} -prev-> {:#x}"
-                             .format(long(prev.address), long(node.address),
-                                     long(node['prev'])))
+                if long(prev.address) != long(node[prev_]):
+                    error = ("broken {} link {:#x} -{}-> {:#x} -{}-> {:#x}"
+                             .format(prev_, long(prev.address), next_, long(node.address),
+                                     prev_, long(node[prev_])))
                     pending_exception = CorruptListError(error)
                     # broken prev link means there might be a cycle that
                     # does not include the initial head, so start detecting
                     # cycles
                     fast = node
-                nxt = node['next']
+                nxt = node[next_]
 
                 if fast is not None:
                     # are we detecting cycles? advance fast 2 times and compare
                     # each with our current node (Floyd's Tortoise and Hare
                     # algorithm)
                     for i in range(2):
-                        fast = fast['next'].dereference()
+                        fast = fast[next_].dereference()
                         if node.address == fast.address:
                             raise ListCycleError("Cycle in list detected.")
 
                 prev = node
                 if long(nxt) == 0:
-                    raise CorruptListError("next pointer is NULL")
+                    raise CorruptListError("{} pointer is NULL".format(next_))
                 node = nxt.dereference()
             except gdb.error as e:
-                raise BufferError("Failed to read list_head {:#x}: {}"
-                                  .format(long(node.address), str(e)))
+                raise BufferError("Failed to read list_head {:#x} in list {:#x}: {}"
+                                  .format(long(node.address), long(list_head.address), str(e)))
         if pending_exception is not None:
             raise pending_exception
 
     @export
-    def list_for_each_entry(self, list_head, gdbtype, member, include_head=False):
-        for node in list_for_each(list_head, include_head=include_head):
+    def list_for_each_entry(self, list_head, gdbtype, member, include_head=False, reverse=False):
+        for node in list_for_each(list_head, include_head=include_head, reverse=reverse):
             if node.type != self.list_head_type.pointer():
                 raise TypeError("Type {} found. Expected struct list_head *."
                                 .format(str(node.type)))
