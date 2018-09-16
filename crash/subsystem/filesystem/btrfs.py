b06e290786b0c570bb0116cbd68045a0149e6960
Jeff Mahoney <jeffm@suse.com>
2018-09-16 08:30:39 -0400

    crash.subsystem.filesystems: add documentation
    Add documentation for public methods.  Rename type registration
    callbacks to make it obvious they are intended to be private.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

--- crash/subsystem/filesystem/btrfs.py
+++ crash/subsystem/filesystem/btrfs.py
@@ -14,8 +14,33 @@ class BtrfsFileSystem(CrashBaseClass):
 
     @classmethod
     def btrfs_inode(cls, vfs_inode):
+        """
+        Converts a VFS inode to a btrfs inode
+
+        This method converts a struct inode to a struct btrfs_inode.
+
+        Args:
+            vfs_inode (gdb.Value<struct inode>): The struct inode to convert
+                to a struct btrfs_inode
+
+        Returns:
+            gdb.Value<struct btrfs_inode>: The converted struct btrfs_inode
+        """
         return container_of(vfs_inode, cls.btrfs_inode_type, 'vfs_inode')
 
     @classmethod
     def btrfs_sb_info(cls, super_block):
+        """
+        Converts a VFS superblock to a btrfs fs_info
+
+        This method converts a struct super_block to a struct btrfs_fs_info
+
+        Args:
+            super_block (gdb.Value<struct super_block>): The struct super_block
+                to convert to a struct btrfs_fs_info.
+
+        Returns:
+            gdb.Value<struct btrfs_fs_info>: The converted struct
+                btrfs_fs_info
+        """
         return super_block['s_fs_info'].cast(cls.btrfs_fs_info_p_type)
--- crash/subsystem/filesystem/ext3.py
+++ crash/subsystem/filesystem/ext3.py
@@ -17,10 +17,10 @@ from crash.subsystem.filesystem import register_buffer_head_decoder
 
 class Ext3(CrashBaseClass):
     __symbol_callbacks__ = [
-            ('journal_end_buffer_io_sync', 'register_journal_buffer_io_sync') ]
+            ('journal_end_buffer_io_sync', '_register_journal_buffer_io_sync') ]
 
     @classmethod
-    def register_journal_buffer_io_sync(cls, sym):
+    def _register_journal_buffer_io_sync(cls, sym):
         # ext3/ext4 and jbd/jbd2 share names but not implementations
         b = gdb.block_for_pc(long(sym.value().address))
         sym = get_symbol_value('journal_end_buffer_io_sync', b)
@@ -29,6 +29,28 @@ class Ext3(CrashBaseClass):
 
     @classmethod
     def decode_journal_buffer_io_sync(cls, bh):
+        """
+        Decodes an ext3 journal buffer
+
+        This method decodes a struct buffer_head with and end_io callback
+        of journal_end_buffer_io_sync.
+
+        Args:
+            bh (gdb.Value<struct buffer_head>): The struct buffer_head to
+                decode
+
+        Returns:
+            dict: Contains the following items:
+                - description (str): Human-readable description of
+                    the buffer head
+                - bh (gdb.Value<struct buffer_head>): The buffer head being
+                    decoded
+                - fstype (str): The name of the file system type being decoded
+                - devname (str): The name of the device the file system uses
+                - offset (long): The offset, in bytes, of the block described
+                - length (long): The length of the block described
+        """
+
         fstype = "journal on ext3"
         devname = block_device_name(bh['b_bdev'])
         chain = {
