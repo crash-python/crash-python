c1467a7157595b2f3d8aab02935dc4590929379d
Jeff Mahoney <jeffm@suse.de>
2019-04-26 21:36:51 +0200

    crash.subsystem.filesystem: add super_flags
    super_flags returns a human-readable string describing the flags
    in super_block->s_flags.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

--- crash/subsystem/filesystem/__init__.py
+++ crash/subsystem/filesystem/__init__.py
@@ -2,11 +2,72 @@
 # vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
 
 import gdb
-from crash.util import container_of
+from crash.util import container_of, decode_flags
 from crash.infra import CrashBaseClass, export
 from crash.types.list import list_for_each_entry
 from crash.subsystem.storage import block_device_name
 from crash.subsystem.storage import Storage as block
+
+MS_RDONLY       = 1
+MS_NOSUID       = 2
+MS_NODEV        = 4
+MS_NOEXEC       = 8
+MS_SYNCHRONOUS  = 16
+MS_REMOUNT      = 32
+MS_MANDLOCK     = 64
+MS_DIRSYNC      = 128
+MS_NOATIME      = 1024
+MS_NODIRATIME   = 2048
+MS_BIND         = 4096
+MS_MOVE         = 8192
+MS_REC          = 16384
+MS_VERBOSE      = 32768
+MS_SILENT       = 32768
+MS_POSIXACL     = (1<<16)
+MS_UNBINDABLE   = (1<<17)
+MS_PRIVATE      = (1<<18)
+MS_SLAVE        = (1<<19)
+MS_SHARED       = (1<<20)
+MS_RELATIME     = (1<<21)
+MS_KERNMOUNT    = (1<<22)
+MS_I_VERSION    = (1<<23)
+MS_STRICTATIME  = (1<<24)
+MS_LAZYTIME     = (1<<25)
+MS_NOSEC        = (1<<28)
+MS_BORN         = (1<<29)
+MS_ACTIVE       = (1<<30)
+MS_NOUSER       = (1<<31)
+
+SB_FLAGS = {
+    MS_RDONLY       : "MS_RDONLY",
+    MS_NOSUID       : "MS_NOSUID",
+    MS_NODEV        : "MS_NODEV",
+    MS_NOEXEC       : "MS_NOEXEC",
+    MS_SYNCHRONOUS  : "MS_SYNCHRONOUS",
+    MS_REMOUNT      : "MS_REMOUNT",
+    MS_MANDLOCK     : "MS_MANDLOCK",
+    MS_DIRSYNC      : "MS_DIRSYNC",
+    MS_NOATIME      : "MS_NOATIME",
+    MS_NODIRATIME   : "MS_NODIRATIME",
+    MS_BIND         : "MS_BIND",
+    MS_MOVE         : "MS_MOVE",
+    MS_REC          : "MS_REC",
+    MS_SILENT       : "MS_SILENT",
+    MS_POSIXACL     : "MS_POSIXACL",
+    MS_UNBINDABLE   : "MS_UNBINDABLE",
+    MS_PRIVATE      : "MS_PRIVATE",
+    MS_SLAVE        : "MS_SLAVE",
+    MS_SHARED       : "MS_SHARED",
+    MS_RELATIME     : "MS_RELATIME",
+    MS_KERNMOUNT    : "MS_KERNMOUNT",
+    MS_I_VERSION    : "MS_I_VERSION",
+    MS_STRICTATIME  : "MS_STRICTATIME",
+    MS_LAZYTIME     : "MS_LAZYTIME",
+    MS_NOSEC        : "MS_NOSEC",
+    MS_BORN         : "MS_BORN",
+    MS_ACTIVE       : "MS_ACTIVE",
+    MS_NOUSER       : "MS_NOUSER",
+}
 
 class FileSystem(CrashBaseClass):
     __types__ = [ 'struct dio *',
@@ -37,7 +98,7 @@ class FileSystem(CrashBaseClass):
 
     @export
     @staticmethod
-    def super_fstype(sb):
+    def super_fstype(sb: gdb.Value) -> str:
         """
         Returns the file system type's name for a given superblock.
 
@@ -49,6 +110,22 @@ class FileSystem(CrashBaseClass):
             str: The file system type's name
         """
         return sb['s_type']['name'].string()
+
+    @export
+    @staticmethod
+    def super_flags(sb: gdb.Value) -> str:
+        """
+        Returns the flags associated with the given superblock.
+
+        Args:
+            sb (gdb.Value<struct super_block>): The struct super_block for
+                which to return the flags.
+
+        Returns:
+            str: The flags field in human-readable form.
+
+        """
+        return decode_flags(sb['s_flags'], SB_FLAGS)
 
     @export
     @classmethod
