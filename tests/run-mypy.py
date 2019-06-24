ef874ed85cebc5b9d01b1b7764b288904dbfecc5
Jeff Mahoney <jeffm@suse.com>
2019-06-24 18:52:34 -0400

    tests: specify stdout/stderr for mypy.main
    The version of mypy in OBS requires stdout/stderr params for main.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

--- tests/run-mypy.py
+++ tests/run-mypy.py
@@ -14,8 +14,8 @@ common_args = ["--ignore-missing-imports",
                "--check-untyped-defs",
                "--disallow-untyped-globals"]
 
-ret = main(None, args=["-p", "kdump"] + common_args)
-ret2 = main(None, args=["-p", "crash"] + common_args)
+ret = main(None, stdout=sys.stdout, stderr=sys.stderr, args=["-p", "kdump"] + common_args)
+ret2 = main(None, stdout=sys.stdout, stderr=sys.stderr, args=["-p", "crash"] + common_args)
 
 if ret or ret2:
     print("static checking failed.", file=sys.stderr)
