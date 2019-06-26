28c0e973d7fd9ef47b576a67e5739dc9c8ff01d7
Jan Kara <jack@suse.cz>
2019-06-26 12:28:35 +0200

    crash.subsystem.storage: Add checks for queue type in sq functions
    Add checks to functions for single-queue queues that they are indeed
    operating on such queues.
    
    Signed-off-by: Jan Kara <jack@suse.cz>

--- crash/subsystem/storage/blocksq.py
+++ crash/subsystem/storage/blocksq.py
@@ -5,6 +5,8 @@ from typing import Iterable, Tuple
 
 from crash.util.symbols import Types
 from crash.types.list import list_for_each_entry
+from crash.subsystem.storage import queue_is_mq
+from crash.exceptions import InvalidArgumentError
 
 import gdb
 
@@ -12,6 +14,10 @@ class NoQueueError(RuntimeError):
     pass
 
 types = Types(['struct request'])
+
+def _check_queue_type(queue: gdb.Value) -> None:
+    if queue_is_mq(queue):
+        raise InvalidArgumentError("Passed request queue is a multiqueue queue")
 
 def sq_for_each_request_in_queue(queue: gdb.Value) -> Iterable[gdb.Value]:
     """
@@ -30,6 +36,7 @@ def sq_for_each_request_in_queue(queue: gdb.Value) -> Iterable[gdb.Value]:
     """
     if int(queue) == 0:
         raise NoQueueError("Queue is NULL")
+    _check_queue_type(queue)
     return list_for_each_entry(queue['queue_head'], types.request_type,
                                'queuelist')
 
@@ -46,5 +53,6 @@ def sq_requests_in_flight(queue: gdb.Value) -> Tuple[int, int]:
         the 2-tuple is the number of read requests, the second is the number
         of write requests.
     """
+    _check_queue_type(queue)
     return (int(queue['in_flight'][0]),
             int(queue['in_flight'][1]))
