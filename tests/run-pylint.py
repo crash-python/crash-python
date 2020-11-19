9e2c54ed0ec4f1f64397c72f48308befd4795b64
Jeffrey Mahoney <jeffm@suse.com>
2020-11-19 14:31:12 -0500

    pylint: define sys.executable as /usr/bin/python3
    gdb sets its own sys.executable as /usr/bin/python regardless of whether
    python 2 or 3 is used.  Pylint tries to exec the interpreter and fails
    when it tries to use python 2 on python 3 code.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

--- tests/run-pylint.py
+++ tests/run-pylint.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 # vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
 
+# gdb sets sys.executable as python regardless of whether it's python2 or 3
+import sys
+sys.executable = "/usr/bin/python3"
+
 from pylint import lint
 import os
 import shlex
