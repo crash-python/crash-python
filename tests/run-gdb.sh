3bb193efec2881ac04a7b796f2ba495f7ff98f39
Jeff Mahoney <jeffm@suse.com>
2022-07-22 16:03:42 -0400

    tests: allow use of custom gdb and command line
    Similar to running pycrash with a gdb under development, being able to
    use it to run the tests is helpful as well.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

--- tests/run-gdb.sh
+++ tests/run-gdb.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 DIR=$(dirname "$0")
+
+if test -z "$GDB"; then
+	GDB=crash-python-gdb
+fi
+
 echo "Starting gdb"
-exec crash-python-gdb -nx -batch -x $DIR/gdbinit-boilerplate "$@"
+exec $GDB $GDB_CMDLINE -nx -batch -x $DIR/gdbinit-boilerplate "$@"
