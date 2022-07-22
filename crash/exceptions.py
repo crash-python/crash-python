ce0e6ef865c44f4df80a1cfbf1f78bdff206a3d7
Jeff Mahoney <jeffm@suse.com>
2022-07-22 11:44:21 -0400

    crash.types.task: Update for Linux 5.14
    Linux 5.14 repurposed the task_struct::state field.  As part of the
    change, the field was renamed to __state.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

--- crash/exceptions.py
+++ crash/exceptions.py
@@ -17,6 +17,9 @@ class MissingSymbolError(RuntimeError):
 class MissingTypeError(RuntimeError):
     """The requested type cannot be located."""
 
+class MissingFieldError(RuntimeError):
+    """The requested field cannot be located."""
+
 class CorruptedError(RuntimeError):
     """A corrupted data structure has been encountered."""
 
