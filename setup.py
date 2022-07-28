02332747e595444c196adb8d13af0def424d63cc
Jeff Mahoney <jeffm@suse.com>
2022-07-28 13:27:46 -0400

    crash.addrxlat: align with newest version
    The type=enum argument has been replaced with an os_type=str argument.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     },
     python_requires='>=3.6',
 
-    install_requires = [ 'pyelftools' ],
+    install_requires = [ 'pyelftools', 'addrxlat' ],
 
     author = "Jeff Mahoney",
     author_email = "jeffm@suse.com",
